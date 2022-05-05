0993db8c86e65e9c54063c529912d9745e392d4b
Vitaliy <nezhinskiy22@gmail.com>
2022-05-05 21:02:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Switch, Route, Link } from "react-router-dom";
+import { Button, Layout, Space, Typography } from "antd";
+import { Navbar } from "./components";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Avatar, Button, Menu, Typography } from "antd";
+import { Link } from "react-router-dom";
+import {
+  BulbOutlined,
+  FundOutlined,
+  HomeOutlined,
+  MenuOutlined,
+  MoneyCollectOutlined,
+} from "@ant-design/icons";
+
+import icon from "../images/cryptocurrency.png";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* <Button className="menu-control-container" >
+
+        </Button> */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
